fae669cda65d42af81cace3b9f553c7f96bef1e7
jfgarcia0911 <jfgarcia0911@gmail.com>
2025-09-18 00:53:43 +0800

    feat: setup root layout with AuthProvider and ToastProvider

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import {AuthProvider}  from "./context/AuthContext ";
+import ToastProvider from "./ui/ToastProvider"; 
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -26,6 +28,7 @@ export default function RootLayout({ children }) {
       >
         <AuthProvider>
           {children}
+           <ToastProvider /> 
         </AuthProvider>
       </body>
 
