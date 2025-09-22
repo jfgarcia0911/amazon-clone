e1747fdc100c071d0acb970528496e5713f2135e
jfgarcia0911 <jfgarcia0911@gmail.com>
2025-09-22 17:00:12 +0800

    fix: add user dependency to useEffect hook in Hero component

--- app/components/home/Hero.js
+++ app/components/home/Hero.js
@@ -13,55 +13,57 @@ export default function Hero() {
 	const { user } = useAuth();
 
 	useEffect(() => {
-		if(user){
+		if (user) {
 			// Retrieve recently viewed items from localStorage
-		const storedItems = JSON.parse(localStorage.getItem("recentView"));
-		setRecentItems(storedItems.slice(0, 20));
+			const storedItems = JSON.parse(localStorage.getItem("recentView"));
+			setRecentItems(storedItems.slice(0, 20));
 
-		const relatedItems = JSON.parse(
-			localStorage.getItem("relatedCategory")
-		);
-	const fetchProductsByCategory = async (cat) => {
-			try {
-				const q = query(
-					collection(db, "amazon-products"),
-					where("category", "==", cat),
-					orderBy("timestamps.createdAt", "desc")
-				);
-				const querySnapshot = await getDocs(q);
-				const items = querySnapshot.docs.map((doc) => ({
-					id: doc.id,
-					name: doc.data().name,
-					mainImage: doc.data().images.mainImage,
-				}));
-				console.log(items);
-				setRelatedItems(items)
-			} catch (err) {
-				console.error("Error fetching products:", err);
+			const relatedItems = JSON.parse(
+				localStorage.getItem("relatedCategory")
+			);
+			const fetchProductsByCategory = async (cat) => {
+				try {
+					const q = query(
+						collection(db, "amazon-products"),
+						where("category", "==", cat),
+						orderBy("timestamps.createdAt", "desc")
+					);
+					const querySnapshot = await getDocs(q);
+					const items = querySnapshot.docs.map((doc) => ({
+						id: doc.id,
+						name: doc.data().name,
+						mainImage: doc.data().images.mainImage,
+					}));
+					console.log(items);
+					setRelatedItems(items);
+				} catch (err) {
+					console.error("Error fetching products:", err);
+				}
+			};
+
+			if (relatedItems) {
+				fetchProductsByCategory(relatedItems);
 			}
-		};
-
-		if (relatedItems) {
-			fetchProductsByCategory(relatedItems);
 		}
-		}
-	
-	}, []);
+	}, [user]);
 
 	return (
 		<div className="flex flex-col mt-26">
 			<ProductCarousel />
 			<FeaturedProductsGrid />
-			
-			{recentItems.length > 0  && <ProductOptions
-				images={recentItems}
-				title={"Recent viewed items"}
-			/>}
-			{relatedItems.length > 0 && <ProductOptions
-				images={relatedItems}
-				title={"Related to items you've viewed"}
-			/> }
-			
+
+			{recentItems.length > 0 && (
+				<ProductOptions
+					images={recentItems}
+					title={"Recent viewed items"}
+				/>
+			)}
+			{relatedItems.length > 0 && (
+				<ProductOptions
+					images={relatedItems}
+					title={"Related to items you've viewed"}
+				/>
+			)}
 		</div>
 	);
 }
