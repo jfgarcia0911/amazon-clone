d20ade0917759b4a3b4fdb8544d9857f4d74fc0d
jfgarcia0911 <jfgarcia0911@gmail.com>
2026-03-17 03:50:53 +0800

    style: add responsive visibility classes to DeliverTo

--- app/components/header/DeliverTo.js
+++ app/components/header/DeliverTo.js
@@ -3,7 +3,7 @@ import { MapPin, ShoppingCart } from "lucide-react";
 
 export default function DeliverTo() {
 	return (
-		<div className="text-xs relative w-[110px] leading-none border border-transparent  hover:border-white px-1 py-2 cursor-pointer">
+		<div className="hidden lg:block text-xs relative leading-none border border-transparent  hover:border-white px-1 py-2 cursor-pointer">
 			<MapPin size={20} className="absolute top-4 left-1 " />
 			<div className="ml-5">Deliver to</div>
 			<span className="text-sm font-bold block ml-5 ">Philippines</span>
