58ef01ba298ff7e86fd75bdb7c2e6bd0540dec49
jfgarcia0911 <jfgarcia0911@gmail.com>
2026-03-18 03:16:35 +0800

    fix(header): enable exit animation by restructuring AnimatePresence usage

--- app/components/layout/Header.js
+++ app/components/layout/Header.js
@@ -115,120 +115,135 @@ export default function Header() {
 			</div>
 
 			{/* Open modal */}
-			{open && (
-				<div className="fixed top-0 w-full text-white  z-50">
-					<div className="flex w-full">
-						{/* Left Navigation */}
-						<div className="w-full bg-white h-screen overflow-y-auto">
-							<div className="  bg-gray-800 px-4 pt-5 pb-3">
-								{user ? (
-									<div className="flex justify-end">
-                    <h1 className=" mr-1">{user.displayName}</h1>
-                  </div>
-								) : (
-									<div
-										onClick={handleSignin}
-										className="flex items-center cursor-pointer"
-									>
-										<h1 className="ml-auto mr-1">Sign in</h1>
-										<UserRound size={25} className=" " />
+			<AnimatePresence>
+				{open && (
+					<motion.div
+						initial={{ opacity: 0, scaleX: 0 }}
+						animate={{ opacity: 1, scaleX: 1 }}
+						exit={{ opacity: 0, scaleX: 0 }}
+						transition={{ duration: 1, ease: "easeInOut" }}
+						style={{ transformOrigin: "left" }}
+						className="fixed top-0 w-full text-white  z-50"
+					>
+						<div className="flex w-full">
+							{/* Left Navigation */}
+
+							<div className="w-full bg-white h-screen overflow-y-auto">
+								<div className="  bg-gray-800 px-4 pt-5 pb-3">
+									{user ? (
+										<div className="flex justify-end">
+											<h1 className=" mr-1">{user.displayName}</h1>
+										</div>
+									) : (
+										<div
+											onClick={handleSignin}
+											className="flex items-center cursor-pointer"
+										>
+											<h1 className="ml-auto mr-1">Sign in</h1>
+											<UserRound size={25} className=" " />
+										</div>
+									)}
+									<div>
+										<h2 className="font-bold text-lg">Browse </h2>
+										<h1 className="font-normal text-2xl -mt-2">Amazon</h1>
 									</div>
-								)}
-								<div>
-									<h2 className="font-bold text-lg">Browse </h2>
-									<h1 className="font-normal text-2xl -mt-2">Amazon</h1>
 								</div>
-							</div>
-							{/* First Section */}
-							<div className="text-black px-4 font-bold text-lg">
-								<div
-									onClick={handleClick}
-									className="flex justify-between py-3"
-								>
+								{/* First Section */}
+								<div className="text-black px-4 font-bold text-lg">
+									<div
+										onClick={handleClick}
+										className="flex justify-between py-3"
+									>
+										<h1>Amazon Home</h1>
+										<Home size={25} className=" cursor-pointer" />
+									</div>
+								</div>
+								{/* Line */}
+								<div className="h-1 bg-gray-300"></div>
+								{/* Second Section */}
+								<div className="text-black px-4 font-bold text-lg py-3">
 									<h1>Amazon Home</h1>
-									<Home size={25} className=" cursor-pointer" />
+									<h2 className="font-normal text-sm mt-5">Movers & Shakers</h2>
+								</div>
+								{/* Line */}
+								<div className="h-1 bg-gray-300"></div>
+								{/* Third Section */}
+								<div className="text-black px-4 font-bold text-lg py-3 h-full">
+									<h1>Top Departments</h1>
+									<h2 className="font-normal text-sm mt-5">Home</h2>
+									<h2 className="font-normal text-sm mt-5">
+										Health & Household
+									</h2>
+									<h2 className="font-normal text-sm mt-5">Books </h2>
+									<h2 className="font-normal text-sm mt-5">PC </h2>
+
+									<AnimatePresence>
+										{!expand && (
+											<motion.div
+												initial={{ opacity: 0, scaleY: 0 }}
+												animate={{ opacity: 1, scaleY: 1 }}
+												exit={{ opacity: 0, scaleY: 0 }}
+												transition={{ duration: 0.3, ease: "easeInOut" }}
+												style={{ transformOrigin: "bottom" }}
+												onClick={() => setExpand(!expand)}
+												className="flex items-center mt-5  cursor-pointer"
+											>
+												<h2 className="font-normal text-sm mr-1">See all </h2>
+												<ChevronDown size={20} />
+											</motion.div>
+										)}
+									</AnimatePresence>
+									<AnimatePresence>
+										{expand && (
+											<motion.div
+												initial={{ opacity: 0, scaleY: 0 }}
+												animate={{ opacity: 1, scaleY: 1 }}
+												exit={{ opacity: 0, scaleY: 0 }}
+												transition={{ duration: 1.3, ease: "easeInOut" }}
+												style={{ transformOrigin: "top" }}
+												className="overflow-hidden"
+											>
+												{/* Line */}
+												<div className="border mt-5  border-gray-300"></div>
+
+												{items.map((item, index) => (
+													<div
+														key={index}
+														className="flex justify-between font-normal text-sm mt-5"
+													>
+														<h1>{item}</h1>
+														<ChevronRight
+															size={20}
+															className=" cursor-pointer"
+														/>
+													</div>
+												))}
+
+												<div
+													onClick={() => setExpand(!expand)}
+													className="flex items-center mt-5 cursor-pointer mb-5"
+												>
+													<h2 className="font-normal text-sm mr-1  ">
+														See less{" "}
+													</h2>
+													<ChevronUp size={20} />
+												</div>
+											</motion.div>
+										)}
+									</AnimatePresence>
 								</div>
 							</div>
-							{/* Line */}
-							<div className="h-1 bg-gray-300"></div>
-							{/* Second Section */}
-							<div className="text-black px-4 font-bold text-lg py-3">
-								<h1>Amazon Home</h1>
-								<h2 className="font-normal text-sm mt-5">Movers & Shakers</h2>
-							</div>
-							{/* Line */}
-							<div className="h-1 bg-gray-300"></div>
-							{/* Third Section */}
-							<div className="text-black px-4 font-bold text-lg py-3 h-full">
-								<h1>Top Departments</h1>
-								<h2 className="font-normal text-sm mt-5">Home</h2>
-								<h2 className="font-normal text-sm mt-5">Health & Household</h2>
-								<h2 className="font-normal text-sm mt-5">Books </h2>
-								<h2 className="font-normal text-sm mt-5">PC </h2>
-
-								<AnimatePresence>
-									{!expand && (
-										<motion.div
-											initial={{ opacity: 0, scaleY: 0 }}
-											animate={{ opacity: 1, scaleY: 1 }}
-											exit={{ opacity: 0, scaleY: 0 }}
-											transition={{ duration: 0.3, ease: "easeInOut" }}
-											style={{ transformOrigin: "top" }}
-											onClick={() => setExpand(!expand)}
-											className="flex items-center mt-5  cursor-pointer"
-										>
-											<h2 className="font-normal text-sm mr-1">See all </h2>
-											<ChevronDown size={20} />
-										</motion.div>
-									)}
-								</AnimatePresence>
-								<AnimatePresence>
-									{expand && (
-										<motion.div
-											initial={{ opacity: 0, scaleY: 0 }}
-											animate={{ opacity: 1, scaleY: 1 }}
-											exit={{ opacity: 0, scaleY: 0 }}
-											transition={{ duration: 1.3, ease: "easeInOut" }}
-											style={{ transformOrigin: "top" }}
-											className="overflow-hidden"
-										>
-											{/* Line */}
-											<div className="border mt-5  border-gray-300"></div>
-
-											{items.map((item, index) => (
-												<div
-													key={index}
-													className="flex justify-between font-normal text-sm mt-5"
-												>
-													<h1>{item}</h1>
-													<ChevronRight size={20} className=" cursor-pointer" />
-												</div>
-											))}
-
-											<div
-												onClick={() => setExpand(!expand)}
-												className="flex items-center mt-5 cursor-pointer mb-5"
-											>
-												<h2 className="font-normal text-sm mr-1  ">
-													See less{" "}
-												</h2>
-												<ChevronUp size={20} />
-											</div>
-										</motion.div>
-									)}
-								</AnimatePresence>
+							{/* Right Navigation */}
+							<div
+								onClick={() => setOpen(!open)}
+								className="bg-black/80 min-h-screen flex justify-center  w-20"
+							>
+								<X size={30} className="mt-10 cursor-pointer" />
 							</div>
 						</div>
-						{/* Right Navigation */}
-						<div
-							onClick={() => setOpen(!open)}
-							className="bg-black/80 min-h-screen flex justify-center  w-20"
-						>
-							<X size={30} className="mt-10 cursor-pointer" />
-						</div>
-					</div>
-				</div>
-			)}
+					</motion.div>
+				)}
+			</AnimatePresence>
 		</div>
 	);
 }
