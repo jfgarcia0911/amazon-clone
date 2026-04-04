c2156a08b22248e3682d1d08b416d6563db94b9d
jfgarcia0911 <jfgarcia0911@gmail.com>
2026-04-05 03:23:18 +0800

    fix: update Logo component hover border and spacing

--- app/components/header/Logo.js
+++ app/components/header/Logo.js
@@ -8,10 +8,10 @@ export default function Logo() {
 	return (
 		<div
 			onClick={() => router.push("/")}
-			className="relative w-[112px]  flex items-center justify-center hover:border-1 py-2 px-1  cursor-pointer"
+			className="relative w-[112px]  flex items-center justify-center border border-transparent hover:border-white py-3 px-1  cursor-pointer"
 		>
 			<h1 className="text-3xl font-bold -mt-4">amazon</h1>
-			<div className="absolute top-5 left-2">
+			<div className="absolute top-6 left-2">
 				<Image
 					src="/amazon.png"
 					alt="Logo"
